d77583cea90db525df1022a6b65a46b3ea280e89
jaroslav <jaroslav.ruzicka@hotmail.com>
2018-06-12 08:37:21 +0200

    add ParameterSet constructor with parameters parameter

---
+++ AdoSession/AdoSession.Tests/ParameterSetTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using RoseByte.AdoSession.Interfaces;
+
+namespace RoseByte.AdoSession.Tests
+{
+    [TestFixture]
+    public class ParameterSetTests
+    {
+        [Test]
+        public void ShouldCreateWithParameters()
+        {
+            var parameters = new List<IParameter>
+            {
+                new Mock<IParameter>().Object,
+                new Mock<IParameter>().Object,
+                new Mock<IParameter>().Object
+            };
+            
+            var sut = new ParameterSet(parameters);
+            
+            Assert.That(sut.Count, Is.EqualTo(3));
+        }
+    }
+}
--- AdoSession/AdoSession/ParameterSet.cs
+++ AdoSession/AdoSession/ParameterSet.cs
@@ -4,5 +4,15 @@ using RoseByte.AdoSession.Interfaces;
 namespace RoseByte.AdoSession
 {
     public class ParameterSet : HashSet<IParameter>
-    { }
+    {
+        public ParameterSet(){ }
+        
+        public ParameterSet(IEnumerable<IParameter> parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                Add(parameter);
+            }
+        }
+    }
 }
